d7a1508e0ca8b410e595f1bb19fc4c105d3a593f
vlad <vladkoip13@gmail.com>
2025-03-21 22:32:56 +0400

    feat: added footer

--- src/widgets/TheFooter/TheFooter.vue
+++ src/widgets/TheFooter/TheFooter.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
 import { VButton } from '@/shared/ui';
+import IconInfo from '@/shared/ui/Icons/IconInfo.vue';
+import IconQuestionBtn from '@/shared/ui/Icons/IconQuestionBtn.vue';
 </script>
 
 <template>
-  <footer>
-    <div class="container">
-      <VButton variant="outline">qweqwe</VButton>
-      <VButton variant="primary">zxczxc</VButton>
+  <footer class="footer">
+    <div class="footer__inner container">
+      <div class="footer__body">
+        <VButton variant="primary" big>
+          <IconInfo />
+          О компании
+        </VButton>
+        <VButton variant="primary" big>
+          <IconQuestionBtn />
+          Помощь
+        </VButton>
+      </div>
     </div>
   </footer>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.footer {
+  &__inner {
+    width: 100%;
+    margin-bottom: 40px;
+  }
+
+  &__body {
+    flex: 1;
+    width: 100%;
+    display: flex;
+    gap: 20px;
+  }
+}
+</style>
